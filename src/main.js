a901f0d1061d9f899082a7acf7200e3b0d26f4a0
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-05-04 00:10:04 +0200

    Add mobile menu with interactions

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+Alpine.data('flyout', () => ({
+  open: false,
+
+  toggle() {
+    this.open = !this.open
+  },
+}))
+
 Alpine.start()
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       'medium-gray': 'hsl(0, 0%, 41%)',
       'almost-black': 'hsl(0, 0%, 8%)',
       white: 'hsl(0, 0%, 100%)',
+      transparent: 'transparent'
     },
   },
   plugins: [],
