fc1823fa533e2e4e0ced9c1cf8cea32cfb15eb39
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-08-03 22:48:08 +0200

    Add main.js to tailwind.config to search classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.html', './src/main.js'],
   theme: {
     fontFamily: {
       sans: ['Epilogue', 'san-serif'],
